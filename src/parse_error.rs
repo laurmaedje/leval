06c2a8c297221bea2d1844aab7e211d12abc6f5d
Laurenz <laurmaedje@gmail.com>
2017-01-20 11:22:27 +0100

    use term structure from math_graph

--- src/parse_error.rs
+++ src/parse_error.rs
@@ -3,38 +3,30 @@ use std::fmt;
 /// Representation of any Error that can occur while parsing.
 /// # Examples
 /// ```
-/// # use eval::{evaluate, ParseError};
-/// let err1 = evaluate("4+?");
+/// # use eval::{Term, ParseError};
+/// let err1 = Term::new("4+?");
 /// assert_eq!(err1, Err(ParseError::UnknownSymbol));
+/// ```
+/// ```
+/// # use eval::{Term, ParseError};
+/// let err3 = Term::new("(4+2]*3");
+/// assert_eq!(err3, Err(ParseError::UnbalancedParens));
 ///
-/// let err2 = evaluate("4+6+");
-/// assert_eq!(err2, Err(ParseError::NumberExpected));
-/// ```
-/// ```
-/// # use eval::{evaluate, ParseError};
-/// let err3 = evaluate("(4+2]*3");
-/// assert_eq!(err3, Err(ParseError::WrongClosingParen));
-///
-/// let err4 = evaluate("3*(5+2");
-/// assert_eq!(err4, Err(ParseError::MissingClosingParen));
-/// ```
-/// ```
-/// # use eval::{evaluate, ParseError};
-/// let err5 = evaluate("y+3");
-/// assert_eq!(err5, Err(ParseError::UnknownVariable));
+/// let err4 = Term::new("3*(5+2");
+/// assert_eq!(err4, Err(ParseError::UnbalancedParens));
 /// ```
 #[derive(Debug, PartialEq, Clone)]
 pub enum ParseError {
     /// An unknown symbol appeared
     UnknownSymbol,
     /// A number is missing
-    NumberExpected,
-    /// A parenthesized expression is not closed by a closing paren
-    MissingClosingParen,
-    /// A parenthesized expression started with one paren and is closed by a different one
-    WrongClosingParen,
+    FactorExpected,
+    /// A parenthesized expression is not closed by a closing paren or there are two much opening parens
+    UnbalancedParens,
     /// A used variable was not defined
     UnknownVariable,
+    /// This operation is not defined
+    UnknownOperation,
 }
 
 impl fmt::Display for ParseError {
